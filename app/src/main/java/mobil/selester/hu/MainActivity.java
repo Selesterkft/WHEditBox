c8036e2bfb363b266e61e66ec9a507d4a1fa6f9d
Mészáros Attila Gábor <meszarosag@gmail.com>
2019-03-04 21:46:30 +0100

    Completed

--- app/src/main/java/mobil/selester/hu/MainActivity.java
+++ app/src/main/java/mobil/selester/hu/MainActivity.java
@@ -3,6 +3,9 @@ package mobil.selester.hu;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import mobil.selester.wheditbox.WHEditBox;
 
@@ -12,18 +15,56 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         WHEditBox ceb = findViewById(R.id.customEB);
-        ceb.setDialogBox(this);
-        ceb.setSuffix("#&");
-        ceb.EDText.setText("proba");
+        WHEditBox ceb1 = findViewById(R.id.customEB1);
+        WHEditBox ceb2 = findViewById(R.id.customEB2);
+        WHEditBox ceb3 = findViewById(R.id.customEB3);
+
+        // ------------------ Global definition ----------------
+
+        WHEditBox.activity = this;
+        WHEditBox.suffix = "#&";
+
+        //------------------------------------------------------
+
+        ceb.setNextFocus(ceb1.EDText);
+        ceb1.setNextFocus(ceb2.EDText);
+        ceb2.setNextFocus(ceb3.EDText);
+
+        ceb.EDText.setText("None");
+        ceb.setDialogTitle("Valami");
         ceb.setOnDetectBarcodeListener(new WHEditBox.OnDetectBarcodeListener() {
             @Override
             public void OnDetectBarcode() {
-                Log.i("TAG","OK");
+                Toast.makeText(getBaseContext(),"OK",Toast.LENGTH_LONG).show();
             }
         });
-        WHEditBox ceb1 = findViewById(R.id.customEB1);
-        ceb1.setDialogBox(this);
+
+        ceb1.EDText.setText("FocusDialog");
+        ceb1.setOnDetectBarcodeListener(new WHEditBox.OnDetectBarcodeListener() {
+            @Override
+            public void OnDetectBarcode() {
+                Toast.makeText(getBaseContext(),"OK1",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        ceb2.EDText.setText("ClickDialog");
+        ceb2.setOnDetectBarcodeListener(new WHEditBox.OnDetectBarcodeListener() {
+            @Override
+            public void OnDetectBarcode() {
+                Toast.makeText(getBaseContext(),"OK2",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        ceb3.EDText.setText("ClickKeyboard");
+        ceb3.setOnDetectBarcodeListener(new WHEditBox.OnDetectBarcodeListener() {
+            @Override
+            public void OnDetectBarcode() {
+                Toast.makeText(getBaseContext(),"OK3",Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 
 }
--- wheditbox/src/main/java/mobil/selester/wheditbox/WHEditBox.java
+++ wheditbox/src/main/java/mobil/selester/wheditbox/WHEditBox.java
@@ -16,13 +16,17 @@ import android.text.TextWatcher;
 import android.util.AttributeSet;
 import android.content.res.TypedArray;
 import android.util.Log;
+import android.util.TypedValue;
+import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.security.Key;
 
@@ -32,28 +36,31 @@ public class WHEditBox extends LinearLayout {
         public void OnDetectBarcode();
     }
 
-    @StyleableRes
-    int index0 = 0;
-
-    @StyleableRes
-    int index1 = 1;
-
-    @StyleableRes
-    int index2 = 2;
-
-    @StyleableRes
-    int index3 = 3;
-
     ConstraintLayout mainLayout;
     public EditText EDText;
-    ImageView delBtn, zoomBtn;
+    public static String suffix = "";
+    public static Activity activity;
+
+    ImageView delBtn;
     private AlertDialog popup;
-    private Activity activity;
-    private String suffix = "";
     private OnDetectBarcodeListener detect;
     private EditText et;
-    private Fragment frg;
     private int selectBG, BG;
+    private int writeType;
+    private EditText nextET = null;
+    private String dialogTitle = "Adat megadása";
+    private static int WRITETYPE_None = 0;
+    private static int WRITETYPE_ClickDialog = 1;
+    private static int WRITETYPE_FocusDialog = 2;
+    private static int WRITETYPE_ClickKeyboard = 3;
+
+
+    public static int dpToPx(Context context, int dp) {
+        float density = context.getResources()
+                .getDisplayMetrics()
+                .density;
+        return Math.round((float) dp * density);
+    }
 
     public WHEditBox(Context context, AttributeSet attributeSet){
         super(context, attributeSet);
@@ -62,31 +69,22 @@ public class WHEditBox extends LinearLayout {
 
     private void init(Context context, AttributeSet attrs){
         inflate(context, R.layout.layoutcustomview, this);
-
         TypedArray typedArray = context.obtainStyledAttributes(attrs,R.styleable.WHEditBox);
         Log.i("TAG",""+typedArray.getIndexCount());
         BG = typedArray.getResourceId(R.styleable.WHEditBox_backgroundStyle,0);
         boolean setDelBtn = typedArray.getBoolean(R.styleable.WHEditBox_setDelBtn,false);
-        boolean setShowTextBtn = typedArray.getBoolean(R.styleable.WHEditBox_setShowTextBtn,false);
-        boolean setClickShowText = typedArray.getBoolean(R.styleable.WHEditBox_setClickShowDialog,false);
         selectBG = typedArray.getResourceId(R.styleable.WHEditBox_selectBackgroundStyle,0);
+        int textSize = typedArray.getDimensionPixelSize(R.styleable.WHEditBox_textSize,0);
+        int innerPadding = typedArray.getDimensionPixelSize(R.styleable.WHEditBox_innerPadding,0);
+        writeType = typedArray.getInt(R.styleable.WHEditBox_writeType, WRITETYPE_None);
         typedArray.recycle();
+
         initComponents();
-        setSelectBackgroundFunc(BG);
+        if( BG > 0) setSelectBackgroundFunc(BG);
         setDelBtnFunc( setDelBtn );
-        setShowTextBtnFunc( setShowTextBtn );
-        setClickShowTextFunc( setClickShowText );
+        if(textSize > 0) EDText.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize);
+        setInnerPadding(innerPadding);
     }
-
-    public void setLocalFragment( Fragment frg ){
-
-    }
-
-    private void setNextTabPositionFunc(Context context, int value) {
-        //EditText et = ((Activity)context).findViewById(value);
-        //et.requestFocus();
-    }
-
 
     public void setOnDetectBarcodeListener(OnDetectBarcodeListener onDetectBarcodeListener){
         detect = onDetectBarcodeListener;
@@ -96,7 +94,6 @@ public class WHEditBox extends LinearLayout {
         mainLayout = findViewById(R.id.mainLayout);
         EDText = findViewById(R.id.editText);
         delBtn = findViewById(R.id.delTextImg);
-        zoomBtn = findViewById(R.id.showTextImg);
         EDText.addTextChangedListener(textWatcher);
         delBtn.setOnClickListener(new OnClickListener() {
             @Override
@@ -104,44 +101,99 @@ public class WHEditBox extends LinearLayout {
                 EDText.getText().clear();
             }
         });
-        setDefaultSetting();
+        setEditTextFunction();
     }
 
-    public void setSuffix(String suffix){
-        this.suffix = suffix;
-    }
+    TextView.OnEditorActionListener onEditorActionListener = new TextView.OnEditorActionListener() {
+        public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+            if ((event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) || (actionId == EditorInfo.IME_ACTION_DONE)) {
+                fireFunction();
+                detect.OnDetectBarcode();
+            }
+            return false;
+        }
+    };
 
-    public String getSuffix(){
-        return suffix;
-    }
 
-    private void setDefaultSetting(){
-        zoomBtn.setOnClickListener(new OnClickListener() {
+    private void setEditTextFunction() {
+        if( writeType != WRITETYPE_None) {
+            EDText.setOnTouchListener(new OnTouchListener(){
+
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    int inType = EDText.getInputType(); // backup the input type
+                    EDText.setInputType(InputType.TYPE_NULL); // disable soft input
+                    EDText.onTouchEvent(event); // call native handler
+                    EDText.setInputType(inType); // restore input type
+                    return true; // consume touch even
+                }
+            });
+        }
+
+        if( writeType == WRITETYPE_ClickKeyboard || writeType == WRITETYPE_ClickDialog || writeType == WRITETYPE_FocusDialog) {
+            EDText.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Log.i("TAG", "CLICK: " + EDText.getText().toString());
+                    if (writeType == WRITETYPE_ClickDialog || writeType == WRITETYPE_FocusDialog) {
+                        showDialog();
+                    } else if (writeType == WRITETYPE_ClickKeyboard) {
+                        Log.i("TAG", "SHOW KEYBOARD");
+                        InputMethodManager imm = (InputMethodManager) EDText.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                        if (imm.isActive()) imm.toggleSoftInput(1, 0);
+                    }
+                }
+            });
+        }
+
+        EDText.setOnFocusChangeListener(new OnFocusChangeListener() {
             @Override
-            public void onClick(View view) {
-                Log.i("TAG",""+EDText.isShown());
-                showDialog();
+            public void onFocusChange(View view, boolean hasFocus) {
+                Log.i("TAG","FOCUS: "+EDText.getText().toString() + ", hasFocus: " + hasFocus );
+
+                activity.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+                InputMethodManager imm = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(EDText.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+
+                if( writeType != WRITETYPE_None ) {
+                    Log.i("TAG","HIDE KEYBOARD");
+
+                    if( writeType == WRITETYPE_FocusDialog ){
+                        if(hasFocus){
+                            showDialog();
+                        }
+                    }
+                }
+                if (selectBG > 0) {
+                    if(hasFocus){
+                        mainLayout.setBackgroundResource(selectBG);
+                    }else{
+                        mainLayout.setBackgroundResource(BG);
+                    }
+                }
             }
         });
-    }
-
-    public void setDialogBox(Activity activity){
-        this.activity = activity;
+        EDText.setOnEditorActionListener(onEditorActionListener);
     }
 
     void showDialog() {
-
         if (activity != null) {
             AlertDialog.Builder builder = new AlertDialog.Builder(activity);
             et = new EditText(getContext());
             et.setText(EDText.getText());
             et.setSelection(et.getText().length());
             et.addTextChangedListener(textWatcher);
+            et.setImeOptions(EditorInfo.IME_ACTION_NEXT);
+            et.setSingleLine(true);
+            et.setOnEditorActionListener(onEditorActionListener);
+
             builder.setView(et);
             builder.setPositiveButton("Megadás", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
                     EDText.setText(et.getText());
+                    InputMethodManager imm = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(popup.getWindow().getDecorView().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
                     popup.dismiss();
                 }
             });
@@ -149,49 +201,47 @@ public class WHEditBox extends LinearLayout {
             builder.setNegativeButton("Mégsem", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
+                    popup.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+                    InputMethodManager imm = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(popup.getWindow().getDecorView().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
                     popup.dismiss();
                 }
             });
             popup = builder.create();
             popup.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
-            popup.setTitle("Adat megadása");
+            popup.setTitle(dialogTitle);
+            popup.setCanceledOnTouchOutside(false);
             popup.show();
-
         } else {
             Log.e("WHEditBox","Activity elérése nincs beállítva!");
         }
     }
 
+    public void setDialogTitle(String dialogTitle){
+
+    }
+
+    public void setNextFocus(EditText et){
+        this.nextET = et;
+    }
+
     //----- Paramaters --------------------------------------
 
-    public void setClickShowTextFunc(boolean value) {
-        if( value ) {
-            EDText.setOnClickListener(new OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Log.i("TAG","CLICK");
-                    showDialog();
-                }
-            });
-            EDText.setOnFocusChangeListener(new OnFocusChangeListener() {
-                @Override
-                public void onFocusChange(View view, boolean hasFocus) {
-                    if(hasFocus){
-                        mainLayout.setBackgroundResource(selectBG);
-                    }else{
-                        mainLayout.setBackgroundResource(BG);
-                    }
-                    Log.i("TAG","FOCUS");
-                    InputMethodManager imm = (InputMethodManager) EDText.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
-                    if (imm.isActive()) imm.toggleSoftInput(0, InputMethodManager.HIDE_NOT_ALWAYS);
-                }
-            });
-        }
+    public void setInnerPadding(int value){
+        EDText.setPadding(value,value,value,value);
     }
+
+    private OnTouchListener otl = new OnTouchListener() {
+        public boolean onTouch (View v, MotionEvent event) {
+            return true; // the listener has consumed the event
+        }
+    };
+
 
     public void setSelectBackgroundFunc(int value) {
         if(value > 0) {
-            mainLayout.setBackgroundResource(value);
+            Log.i("TAG","" + value + ""+dpToPx(getContext(),value));
+            mainLayout.setBackgroundResource( value );
         }
         Log.i("PARAM","setSelectBackground");
     }
@@ -199,25 +249,6 @@ public class WHEditBox extends LinearLayout {
     public void setDelBtnFunc(boolean value) {
         if( value ) delBtn.setVisibility(VISIBLE); else delBtn.setVisibility(GONE);
         Log.i("PARAM","setDelBtnFunc");
-    }
-
-    public void setShowTextBtnFunc(boolean value) {
-        if( value ) zoomBtn.setVisibility(VISIBLE); else zoomBtn.setVisibility(GONE);
-        Log.i("PARAM","setShowTextBtnFunc");
-    }
-
-    public boolean getDelBtnFunc() {
-        if (delBtn.getVisibility() == VISIBLE)
-            return true;
-        else
-            return false;
-    }
-
-    public boolean getShowTextBtnFunc() {
-        if (zoomBtn.getVisibility() == VISIBLE)
-            return true;
-        else
-            return false;
     }
 
 
@@ -242,15 +273,24 @@ public class WHEditBox extends LinearLayout {
                 if (s.length() > suffixLen) {
                     if (s.toString().substring(s.length() - suffixLen, s.length()).equals(suffix)) {
                         EDText.setText(s.toString().substring(0, s.toString().length() - suffixLen));
-                        InputMethodManager imm = (InputMethodManager) et.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
-                        if (imm.isActive()) imm.toggleSoftInput(0, InputMethodManager.HIDE_NOT_ALWAYS);
-                        popup.dismiss();
+                        if( popup != null){
+                            InputMethodManager imm = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+                            imm.hideSoftInputFromWindow(popup.getWindow().getDecorView().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+                            popup.dismiss();
+                        }
+                        fireFunction();
                         detect.OnDetectBarcode();
                     }
                 }
             }
         }
     };
+
+    private void fireFunction(){
+        if( nextET != null ){
+            nextET.requestFocus();
+        }
+    }
 
 
 }
